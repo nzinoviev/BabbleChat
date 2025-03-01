8af5fe324b25c1ea7c0bb38a4d8a8bdcd5482e3c
n.zinoviev <zinoviev.sd@gmail.com>
2025-03-01 15:50:30 +0300

    backcpp: feature: Add StringLib skeleton

---
+++ Servers/Cpp/Src/Include/Library/String.hpp
@@ -0,0 +1,15 @@
+#ifndef __STRING_H__
+#define __STRING_H__
+
+namespace BabbleChat {
+
+class String {
+public:
+
+private:
+
+};
+
+}
+
+#endif // __STRING_H__
---
+++ Servers/Cpp/Src/Library/String/String.cpp
@@ -0,0 +1,1 @@
+#include "Include/Library/String.hpp"
