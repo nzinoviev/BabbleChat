e0725c5b997415704104836d30d00848d63b8f6e
n.zinoviev <zinoviev.sd@gmail.com>
2025-03-27 22:21:50 +0300

    backcpp: test: Add tests for BinaryTree:Iterator

--- Servers/Cpp/Src/Library/BinaryTree/UnitTest/BinaryTreeTest.cpp
+++ Servers/Cpp/Src/Library/BinaryTree/UnitTest/BinaryTreeTest.cpp
@@ -3,47 +3,79 @@
  * @brief   Модульные тесты для класса BinaryTree.
  */
 
-#include <set>
-#include <iostream>
+#include <iostream> // delete me!
 
 #include <gtest/gtest.h>
 
 #include <Include/Library/BinaryTree.hpp>
 
 
-TEST(BinaryTree, Insert_50_40_60_70) {
+TEST(BinaryTreeIterator, EmptyTree) {
    BabbleChat::BinaryTree<int> tree;
 
-   tree.Insert(50);
-   tree.Insert(40);
-   tree.Insert(60);
-   tree.Insert(70);
+   auto beginIt = tree.begin();
+   auto endIt = tree.end();
 
-   EXPECT_EQ(1, 1);
+   EXPECT_TRUE(beginIt == endIt);
 }
 
 
-TEST(BinaryTree, Insert_67_3_100_4_50) {
+TEST(BinaryTreeIterator, InOrderTraversal) {
    BabbleChat::BinaryTree<int> tree;
 
-   tree.Insert(67);
+   tree.Insert(10);
+   tree.Insert(5);
+   tree.Insert(15);
    tree.Insert(3);
-   tree.Insert(100);
-   tree.Insert(50);
-   tree.Insert(4);
+   tree.Insert(7);
+   tree.Insert(12);
+   tree.Insert(17);
 
-   auto it = tree.begin();
-   std::cout << *it << std::endl;
-   ++it;
-   std::cout << *it << std::endl;
-   ++it;
+   // Ожидаемый результат обхода: [ 3, 5, 7, 10, 12, 15, 17 ]
+   std::vector<int> expected = { 3, 5, 7, 10, 12, 15, 17 };
+   std::vector<int> result;
 
-   for (auto it : tree) {
-      std::cout << it << " ";
+   for (auto it = tree.begin(); it != tree.end(); ++it) {
+      result.push_back(*it);
    }
-   std::cout << std::endl;
 
-   EXPECT_EQ(1, 1);
+   EXPECT_EQ(expected, result);
+}
+
+
+TEST(BinaryTreeIterator, OperatorAsterisk) {
+   BabbleChat::BinaryTree<int> tree;
+   tree.Insert(42);
+   auto it = tree.begin();
+
+   EXPECT_EQ(42, *it);
+}
+
+
+TEST(BinaryTreeIterator, OperatorArrow) {
+   BabbleChat::BinaryTree<int> tree;
+   tree.Insert(42);
+   auto it = tree.begin();
+
+   int* ptr = it.operator->();
+   EXPECT_NE(nullptr, ptr);
+   EXPECT_EQ(42, *ptr);
+}
+
+
+TEST(BinaryTreeIterator, EqualityInequalityOperators) {
+   BabbleChat::BinaryTree<int> tree;
+   tree.Insert(10);
+   tree.Insert(5);
+   tree.Insert(15);
+
+   auto it1 = tree.begin();
+   auto it2 = tree.begin();
+   EXPECT_TRUE(it1 == it2);
+
+   ++it1;
+   EXPECT_FALSE(it1 == it2);
+   EXPECT_TRUE(it1 != it2);
 }
 
 
